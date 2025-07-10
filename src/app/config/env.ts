6aef57d04ba9e0f9309ea4355d1465c1f84f4184
Margub Murshed <murshedmargub@gmail.com>
2025-07-10 13:31:47 +0600

    setup completed

---
+++ src/app/config/env.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface envVariablesTypes {
+    PORT: string;
+    DB_URL: string;
+    NODE_ENV: string;
+}
+
+function loadEnvVariables(): envVariablesTypes {
+    const requiredEnvVariables = ["PORT", "DB_URL", "NODE_ENV"];
+    requiredEnvVariables.forEach(key => {
+        if (!process.env[key]) {
+            throw new Error(`Missing required environment variable "${key}"`)
+        }
+    })
+    return {
+        PORT: process.env.PORT as string,
+        DB_URL: process.env.DB_URL as string,
+        NODE_ENV: process.env.NODE_ENV as string
+    }
+}
+
+export const envVariables = loadEnvVariables();
