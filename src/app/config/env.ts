285fa3e98cc897321043775fec500b42f6c6f933
Margub Murshed <murshedmargub@gmail.com>
2025-07-16 18:54:59 +0600

    jwt refresh environment variables added

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -8,13 +8,15 @@ interface envVariablesTypes {
     NODE_ENV: "production" | "development";
     JWT_ACCESS_SECRET: string;
     JWT_ACCESS_EXPIRES: string;
+    JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRES: string;
     BCRYPT_SALT_ROUND: string;
     SUPER_ADMIN_EMAIL: string;
     SUPER_ADMIN_PASS: string;
 }
 
 function loadEnvVariables(): envVariablesTypes {
-    const requiredEnvVariables = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "BCRYPT_SALT_ROUND", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASS"];
+    const requiredEnvVariables = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "BCRYPT_SALT_ROUND","JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASS"];
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
             throw new Error(`Missing required environment variable "${key}"`)
@@ -27,6 +29,8 @@ function loadEnvVariables(): envVariablesTypes {
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
         SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
         SUPER_ADMIN_PASS: process.env.SUPER_ADMIN_PASS as string,
     }
