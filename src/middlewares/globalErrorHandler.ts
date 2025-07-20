ffc7eb71456cc1d3e88bacb5e003541a0a8ec5d2
Margub Murshed <murshedmargub@gmail.com>
2025-07-21 00:53:42 +0600

    Refactor global error handling to improve error response structure and add specific handlers for Mongoose and Zod errors

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -6,37 +6,39 @@ import { envVariables } from "../app/config/env";
 import AppError from "../app/errorHelpers/AppError";
 import { ZodError } from "zod";
 import httpStatus from "http-status-codes";
+import { TErrorSource } from "../app/interfaces/error.types";
+import { handleMongooseDuplicateError } from "../app/helpers/handleMongooseDuplicateError";
+import { handleMongooseValidationError } from "../app/helpers/handleMongooseValidationError";
+import { handleMongooseCastError } from "../app/helpers/handleMongooseCastError";
+import { handleZodError } from "../app/helpers/handleZodError";
 
 export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     let message = "Something went wrong!";
-    let errorSources: any[] = []
+    let errorSources: TErrorSource[] = []
 
-    if(error.code === 11000) {
-        statusCode = httpStatus.CONFLICT;
-        const fieldName = Object.keys(error.keyValue)[0];
-        message = `Duplicate ${fieldName} entered: '${error.keyValue[fieldName]}'. Please use a different value.`;
-    }
-    else if (error.name === "ValidationError") {
-        statusCode = httpStatus.BAD_REQUEST;
-        const validationErrors = Object.values(error.errors);
-        errorSources = validationErrors.map((errorObject: any) => ({
-            path: errorObject.path,
-            message: errorObject.message
-        }));
-        message = "Validation Error";
+    if (error.code === 11000) {
+        const errorObject = handleMongooseDuplicateError(error);
+        statusCode = errorObject.statusCode;
+        message = errorObject.message;
+    } else if (error.name === "ValidationError") {
+        const errorObject = handleMongooseValidationError(error);
+        statusCode = errorObject.statusCode;
+        message = errorObject.message;
+        errorSources = errorObject.errorSources as TErrorSource[];
     } else if (error.name === "CastError") {
-        statusCode = httpStatus.BAD_REQUEST;
-        message = `Invalid MongoDB ${error.path}: '${error.value}'. Provide a valid MongoDB ObjectId value.`;
-    }
-    else if (error instanceof ZodError) {
-        statusCode = httpStatus.BAD_REQUEST;
-        message = error.message
+        const errorObject = handleMongooseCastError(error);
+        statusCode = errorObject.statusCode;
+        message = errorObject.message;
+    } else if (error instanceof ZodError) {
+        const errorObject = handleZodError(error);
+        statusCode = errorObject.statusCode;
+        message = errorObject.message;
+        errorSources = errorObject.errorSources as TErrorSource[];
     } else if (error instanceof AppError) {
         statusCode = error.statusCode;
         message = error.message;
     } else if (error instanceof Error) {
-        statusCode = httpStatus.INTERNAL_SERVER_ERROR;
         message = error.message;
     }
 
@@ -44,6 +46,7 @@ export const globalErrorHandler = (error: any, req: Request, res: Response, next
         success: false,
         message,
         errorSources,
+        error: envVariables.NODE_ENV === "development" ? error : null,
         stack: envVariables.NODE_ENV === "development" ? error?.stack : null
     })
 }
